ab05a13c7da4d10c42ded6549fcec70f2122e372
Andres Caro <38670654+andrescaroc@users.noreply.github.com>
2023-02-19 13:37:34 -0500

    refactor: switch to nginx ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.xlarge"]
 
       min_size     = 1
       max_size     = 3
